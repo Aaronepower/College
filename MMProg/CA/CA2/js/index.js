b588a91083c7eae6620a36d674c98257670aece5
Aaronepower <theaaronepower@gmail.com>
2014-11-10 03:25:36 +0000

    Added canvas funcationality

--- MMProg/CA/CA2/js/index.js
+++ MMProg/CA/CA2/js/index.js
@@ -12,7 +12,7 @@
   imageLoader.queueDownload(imagePaths.negative)
   imageLoader.queueDownload(imagePaths.polaroid)
   imageLoader.downloadAll(function() {
-    var imageOverlay = function(path) {
+    function imageOverlay (path) {
       return function() {
                 var oldImage = canvas.toData
                 context.clearRect(0,0, videoWidth, videoHeight)
@@ -32,32 +32,44 @@
   var video          = document.getElementById('video')
     , canvas         = document.getElementById('canvas')
     , snapShotButton = document.getElementById('snapShotButton')
+    , drawButton     = document.getElementById('drawButton')
+    , eraseButton    = document.getElementById('eraseButton')
 
-  var context          = canvas.getContext('2d')
-    , localMediaStream = null
-    , currentSnap      = new Image() 
-    , videoWidth       = 640
-    , videoHeight      = 480
-    , mouseDown        = false
+  var context            = canvas.getContext('2d')
+    , localMediaStream   = null
+    , currentSnap        = new Image() 
+    , videoWidth         = 640
+    , videoHeight        = 480
+    , previousX          = 0
+    , previousY          = 0
+    , canvasMouseDown    = null
+    , canvasMouseMove    = null
+    , canvasMouseRelease = null
+    , videoSettings      = { video: { mandatory: { maxWidth : videoWidth
+                                                 , maxHeight : videoHeight
+                                                 , minWidth : videoWidth
+                                                 , minHeight : videoHeight
+                                                 }
+                                    }
+                           }
   
-  video.width = videoWidth
-  video.height = videoHeight
-  canvas.width = videoWidth
+  video.width   = videoWidth
+  video.height  = videoHeight
+  canvas.width  = videoWidth
   canvas.height = videoHeight
-  var snapShot = function () {
-        if (localMediaStream) {
-          context.clearRect(0,0, videoWidth, videoHeight)
-          context.drawImage(video, 0, 0)
-          currentSnap.src = canvas.toDataURL()
-        }
+  drawButton.disabled = false
+  eraseButton.disabled = false
+  function snapShot () {
+    if (localMediaStream) {
+      context.clearRect(0,0, videoWidth, videoHeight)
+      context.drawImage(video, 0, 0)
+      currentSnap.src = canvas.toDataURL()
+    }
   }
   
-  var errorCallback = function (e) {
+  function errorCallback (e) { console.log('Error: ', e) }
 
-      console.log('trouble in paradise', e)
-  }
-
-  var successCallback = function (stream) {
+  function successCallback (stream) {
         if (window.URL) {
           video.src = window.URL.createObjectURL(stream)
         }
@@ -68,45 +80,110 @@
         video.play()
   }
 
-  var videoSettings = { video: { mandatory: { maxWidth: videoWidth
-                                            , maxHeight: videoHeight
-                                            }
-                               }
-                      }
+  function getWidthRangeValue () {
+    return document.getElementById('lineWidth').value
+  }
+  function getColor () {
+    return document.getElementById('colorPicker').value
+  }
 
-  var previousX
-    , previousY
-  var lineDraw = function(x, y, draw) {
-        if (draw) {
-          context.beginPath()
-          context.strokeStyle = 'black'
-          context.lineWidth = 5
-          context.moveTo(previousX, previousY)
-          context.lineTo(x, y)
-          context.closePath()
-          context.stroke()
-        }
-        previousX = x
-        previousY = y 
+  function getRectMargins () {
+    var rect = canvas.getBoundingClientRect()
+
+    return { left : rect.left
+           , top : rect.top
+           }
   }
-  var canvasMouseDown = function(event) {
-  var rect = canvas.getBoundingClientRect()
-        lineDraw(event.pageX - rect.left, event.pageY - rect.top, mouseDown)
-        mouseDown = true
+
+  function drawButtonEvent () {
+    var mouseDown = false
+
+    drawButton.disabled  = true
+    eraseButton.disabled = false
+
+    function lineDraw (x, y, draw) {
+          x -= getRectMargins().left
+          y -= getRectMargins().top
+          if (draw) {
+            context.beginPath()
+            context.strokeStyle = getColor()
+            context.lineCap = 'round'
+            context.lineWidth = getWidthRangeValue()
+            context.moveTo(previousX, previousY)
+            context.lineTo(x, y)
+            context.stroke()
+          }
+          previousX = x
+          previousY = y
+    }
+    function drawMouseDown (event) {
+      lineDraw(event.pageX, event.pageY, mouseDown)
+      mouseDown = true
+    }
+    function drawMouseMove (event) {
+      lineDraw(event.pageX, event.pageY, mouseDown)
+    }
+    function drawMouseRelease () {
+      mouseDown = false 
+      context.closePath()
+    }
+
+    changeEvents([drawMouseDown, drawMouseMove, drawMouseRelease])
   }
-  var canvasMouseMove = function(event) {
-  var rect = canvas.getBoundingClientRect()
-        console.log(mouseDown)
-        lineDraw(event.pageX - rect.left, event.pageY - rect.top, mouseDown)
+
+  function eraseButtonEvent () {
+    var mouseDown = false
+    drawButton.disabled = false
+    eraseButton.disabled = true
+
+    function clear (x, y, erase) {
+      x -= getRectMargins().left
+      y -= getRectMargins().top
+      if (clear) {
+        context.beginPath()
+        context.arc(x, y, getWidthRangeValue(), 0, Math.PI * 2)
+        context.clip()
+        context.clearRect(0, 0, videoWidth, videoHeight)
+      }
+    }
+
+    function eraseDownEvent (event) {
+      mouseDown = true
+      clear(event.pageX, event.pageY, mouseDown)
+    }
+
+    function eraseMoveEvent (event) {
+      clear(event.pageX, event.pageY, mouseDown)
+    }
+
+    function eraseReleaseEvent (event) {
+      mouseDown = false
+      context.closePath()
+    }
+
+    changeEvents([eraseDownEvent, eraseMoveEvent, eraseReleaseEvent])
   }
-  var canvasMouseRelease = function(event) {
-        mouseDown = false
+
+  function changeEvents (arrayOfNewEvents) {
+    console.log(eraseButton.click === eraseButtonEvent)
+    console.log(canvasMouseDown);
+    canvas.removeEventListener('mousedown', canvasMouseDown)
+    canvas.removeEventListener('mousemove', canvasMouseMove)
+    canvas.removeEventListener('mouseup', canvasMouseRelease)
+    canvas.removeEventListener('mouseleave', canvasMouseRelease)
+    canvasMouseDown = arrayOfNewEvents[0]
+    canvasMouseMove = arrayOfNewEvents[1]
+    canvasMouseRelease = arrayOfNewEvents[2]
+    console.log(canvasMouseDown)
+    canvas.addEventListener('mousedown', canvasMouseDown)
+    canvas.addEventListener('mousemove', canvasMouseMove)
+    canvas.addEventListener('mouseup', canvasMouseRelease)
+    canvas.addEventListener('mouseleave', canvasMouseRelease)
   }
-  snapButton.addEventListener('click', snapShot, false)
-  canvas.addEventListener('mousedown', canvasMouseDown, false)
-  canvas.addEventListener('mousemove', canvasMouseMove, false)
-  canvas.addEventListener('mouseup', canvasMouseRelease, false)
-  canvas.addEventListener('mouseleave', canvasMouseRelease, false)
+
+  drawButton.addEventListener('click', drawButtonEvent)
+  eraseButton.addEventListener('click', eraseButtonEvent)
+  snapButton.addEventListener('click', snapShot)
   navigator.getUserMedia(videoSettings, successCallback, errorCallback)
 
-})(); 
+})()
