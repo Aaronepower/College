00eb49a30e3e015a29af4a7b48085c6e97511df1
Aaronepower <theaaronepower@gmail.com>
2014-11-10 22:47:42 +0000

    Added undo button

--- MMProg/CA/CA2/js/index.js
+++ MMProg/CA/CA2/js/index.js
@@ -31,9 +31,12 @@
 
   var video          = document.getElementById('video')
     , canvas         = document.getElementById('canvas')
+    , canvasWrapper  = document.getElementById('canvasWrapper')
     , snapShotButton = document.getElementById('snapShotButton')
     , drawButton     = document.getElementById('drawButton')
     , eraseButton    = document.getElementById('eraseButton')
+    , undoButton     = document.getElementById('undoButton')
+    , textButton     = document.getElementById('textButton')
 
   var context            = canvas.getContext('2d')
     , localMediaStream   = null
@@ -42,9 +45,12 @@
     , videoHeight        = 480
     , previousX          = 0
     , previousY          = 0
-    , canvasMouseDown    = null
-    , canvasMouseMove    = null
-    , canvasMouseRelease = null
+    , mouseX             = 0
+    , mouseY             = 0
+    , mouseDown          = false
+    , globalComposite    = 'source-over'
+    , previousCommands   = []
+    , charArray          = []
     , videoSettings      = { video: { mandatory: { maxWidth : videoWidth
                                                  , maxHeight : videoHeight
                                                  , minWidth : videoWidth
@@ -55,7 +61,9 @@
   
   video.width   = videoWidth
   video.height  = videoHeight
-  canvas.width  = videoWidth
+  canvasWrapper.width = videoWidth
+  canvasWrapper.height = videoHeight
+  canvas.width = videoWidth
   canvas.height = videoHeight
   drawButton.disabled = false
   eraseButton.disabled = false
@@ -95,95 +103,120 @@
            }
   }
 
-  function drawButtonEvent () {
-    var mouseDown = false
+  function lineDraw (x, y, globalComposite, draw) {
+    x -= getRectMargins().left
+    y -= getRectMargins().top
+    if (draw) {
+      context.beginPath()
+      context.globalCompositeOperation = globalComposite
+      context.strokeStyle = getColor()
+      context.lineCap = 'round'
+      context.lineWidth = getWidthRangeValue()
+      context.moveTo(previousX, previousY)
+      context.lineTo(x, y)
+      context.stroke()
+    }
+    previousX = x
+    previousY = y
+  }
 
+  function drawButtonEvent () {
+    removeTextEvents()
+    globalComposite      = 'source-over'
     drawButton.disabled  = true
     eraseButton.disabled = false
-
-    function lineDraw (x, y, draw) {
-          x -= getRectMargins().left
-          y -= getRectMargins().top
-          if (draw) {
-            context.beginPath()
-            context.strokeStyle = getColor()
-            context.lineCap = 'round'
-            context.lineWidth = getWidthRangeValue()
-            context.moveTo(previousX, previousY)
-            context.lineTo(x, y)
-            context.stroke()
-          }
-          previousX = x
-          previousY = y
-    }
-    function drawMouseDown (event) {
-      lineDraw(event.pageX, event.pageY, mouseDown)
-      mouseDown = true
-    }
-    function drawMouseMove (event) {
-      lineDraw(event.pageX, event.pageY, mouseDown)
-    }
-    function drawMouseRelease () {
-      mouseDown = false 
-      context.closePath()
-    }
-
-    changeEvents([drawMouseDown, drawMouseMove, drawMouseRelease])
   }
 
   function eraseButtonEvent () {
-    var mouseDown = false
-    drawButton.disabled = false
+    removeTextEvents()
+    globalComposite      = 'destination-out'
+    drawButton.disabled  = false
     eraseButton.disabled = true
-
-    function clear (x, y, erase) {
-      x -= getRectMargins().left
-      y -= getRectMargins().top
-      if (clear) {
-        context.beginPath()
-        context.arc(x, y, getWidthRangeValue(), 0, Math.PI * 2)
-        context.clip()
-        context.clearRect(0, 0, videoWidth, videoHeight)
-      }
-    }
-
-    function eraseDownEvent (event) {
-      mouseDown = true
-      clear(event.pageX, event.pageY, mouseDown)
-    }
-
-    function eraseMoveEvent (event) {
-      clear(event.pageX, event.pageY, mouseDown)
-    }
-
-    function eraseReleaseEvent (event) {
-      mouseDown = false
-      context.closePath()
-    }
-
-    changeEvents([eraseDownEvent, eraseMoveEvent, eraseReleaseEvent])
   }
 
-  function changeEvents (arrayOfNewEvents) {
-    console.log(eraseButton.click === eraseButtonEvent)
-    console.log(canvasMouseDown);
-    canvas.removeEventListener('mousedown', canvasMouseDown)
-    canvas.removeEventListener('mousemove', canvasMouseMove)
-    canvas.removeEventListener('mouseup', canvasMouseRelease)
-    canvas.removeEventListener('mouseleave', canvasMouseRelease)
-    canvasMouseDown = arrayOfNewEvents[0]
-    canvasMouseMove = arrayOfNewEvents[1]
-    canvasMouseRelease = arrayOfNewEvents[2]
-    console.log(canvasMouseDown)
-    canvas.addEventListener('mousedown', canvasMouseDown)
-    canvas.addEventListener('mousemove', canvasMouseMove)
-    canvas.addEventListener('mouseup', canvasMouseRelease)
-    canvas.addEventListener('mouseleave', canvasMouseRelease)
+  function undoButtonEvent () {
+    removeTextEvents()
+    context.clearRect(0, 0, videoWidth, videoHeight)
+    var image = new Image()
+
+    image.onload = function() {
+      if (image.src !== '') context.drawImage(image, 0, 0)
+    }
+    previousCommands.pop()
+    image.src = previousCommands.pop() || ''
   }
 
+  function addTextEvents () {
+    textButton.disabled = true
+    canvasWrapper.addEventListener('click', textClickEvent)
+    canvasWrapper.addEventListener('onkeypress', textKeyPressEvent)
+
+  }
+
+  function removeTextEvents () {
+    if (textButton.disabled) {
+      textButton.disabled = false
+      canvasWrapper.removeEventListener('click', textClickEvent)
+      canvasWrapper.removeEventListener('keypress', textKeyPressEvent)
+
+      canvas.addEventListener('mousedown', canvasMouseDown)
+      canvas.addEventListener('mousemove', canvasMouseMove)
+      canvas.addEventListener('mouseup', canvasMouseUp)
+      canvas.addEventListener('mouseleave', canvasMouseRelease)
+    }
+  }
+
+  function canvasMouseDown (event) {
+    lineDraw(event.pageX, event.pageY, globalComposite, mouseDown)
+    mouseDown = true
+  }
+  function canvasMouseMove (event) {
+    lineDraw(event.pageX, event.pageY, globalComposite, mouseDown)
+  }
+  function canvasMouseRelease () {
+    mouseDown = false
+    context.closePath()
+  }
+
+  function canvasMouseUp () {
+    if (mouseDown) {
+      previousCommands.push(canvas.toDataURL())
+    }
+    canvasMouseRelease()
+  }
+
+  function textClickEvent (event) {
+    console.log('message1')
+    var rect = getRectMargins()
+    mouseX = event.pageX - rect.left
+    mouseY = event.pageY - rect.top
+    charArray = []
+  }
+
+  function textKeyPressEvent (event) {
+    console.log('message');
+    var character = String.fromCharCode(event.which)
+      , width = mouseX
+    if (charArray.length > 0) {
+     charArray.forEach(function (singleChar) {
+       width += context.measureText(singleChar).width
+     }) 
+    }
+    context.fillText(character, width+10, mouseY)
+    charArray.push(character)
+  }
+
+
+  canvas.addEventListener('mousedown', canvasMouseDown)
+  canvas.addEventListener('mousemove', canvasMouseMove)
+  canvas.addEventListener('mouseup', canvasMouseUp)
+  canvas.addEventListener('mouseleave', canvasMouseRelease)
   drawButton.addEventListener('click', drawButtonEvent)
   eraseButton.addEventListener('click', eraseButtonEvent)
+  undoButton.addEventListener('click', undoButtonEvent)
+  textButton.addEventListener('click', addTextEvents)
   snapButton.addEventListener('click', snapShot)
   navigator.getUserMedia(videoSettings, successCallback, errorCallback)
+  drawButtonEvent()
 
 })()
