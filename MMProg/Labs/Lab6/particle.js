a79385c4b1cb0b4c2b31595a361ad0f3677c8fff
Aaronepower <theaaronepower@gmail.com>
2015-03-18 23:07:54 +0000

    did lab 5

--- MMProg/Labs/Lab6/particle.js
+++ MMProg/Labs/Lab6/particle.js
@@ -1,38 +1,46 @@
-function Particle (x, y, speed, radius) {
-  this.x = x
-  this.y = y
-  this.speed = speed 
-  this.velocity =  1+Math.random()*10
-  this.radius = radius
+function Particle () {
+  this.x = Math.random()*canvas.width
+  this.y = 0
+  this.speed = 2+Math.random()*50
+  this.radius = 5+Math.random()*5
   this.colour = getRandomColor()
+  this.shape = Math.random()
 }
 
 Particle.prototype.update = function () {
-  this.y += this.speed * this.velocity
+  this.y += this.speed 
 }
 
 Particle.prototype.kill = function () {
   if(this.y > canvas.height) {
     this.y = 0
-    this.velocity = 1+Math.random()*10
+    this.speed = 2+Math.random()*50
     this.radius = 5+Math.random()*15
     this.colour = getRandomColor()
+    this.shape = Math.random()
   }
 }
 
 Particle.prototype.draw = function (context) {
-  
-  context.beginPath()
-  context.arc(this.x, this.y, this.radius, 0, Math.PI*2)
-  context.closePath()
-  context.fillStyle = this.colour
-  context.fill()
+  if (this.shape > .5) {
+    context.beginPath()
+    context.arc(this.x, this.y, this.radius, 0, Math.PI*2)
+    context.closePath()
+    context.fillStyle = this.colour
+    context.fill()
+  } else {
+    context.beginPath()
+    context.rect(this.x, this.y, this.radius, this.radius)
+    context.closePath()
+    context.fillStyle = this.colour
+    context.fill()
+  }
 }
 function getRandomColor() {
-    var letters = '0123456789ABCDEF'.split('');
-    var color = '#';
+    var letters = '0123456789ABCDEF'.split('')
+    var color = '#'
     for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.floor(Math.random() * 16)];
+        color += letters[Math.floor(Math.random() * 16)]
     }
-    return color;
+    return color
 }
--- MMProg/Labs/Lab6/particleSystem.js
+++ MMProg/Labs/Lab6/particleSystem.js
@@ -1,35 +1,37 @@
 function ParticleSystem (canvas) {
   this.canvas = canvas
-  this.context = this.canvas.getContext("2d")
+  this.context = canvas.getContext("2d")
   this.particles = []
   this.tick = 0
 }
 
 ParticleSystem.prototype.start = function () {
-  this.frameID = window.requestAnimationFrame(this.loop(this.canvas,this.context))
-}
+  if (this.frameID) {
+    this.stop()
+  }
+  this.frameID = window.requestAnimationFrame(loop)
+  var vm = this
+  function loop() {
+    vm.frameID = window.requestAnimationFrame(loop)
+    vm.context.clearRect(0,0,canvas.width,canvas.height) 
 
-ParticleSystem.prototype.loop = function (canvas, context) {
-  return function loop() {
-    console.log('working')
-    window.requestAnimationFrame(loop)
-    context.clearRect(0,0,canvas.width,canvas.height) 
-
-    if(this.tick % 10 == 0) {
-      if(this.particles.length < 100) {
-        this.particles.push(new Particle(Math.random()*canvas.width, 0, 2+Math.random()*3, 5+Math.random()*5))
+    if(vm.tick % 10 == 0) {
+      if(vm.particles.length < 100) {
+        vm.particles.push(new Particle())
       }
     }
 
-    for (var i in this.particles) {
-      this.particles[i].update()
-      this.particles[i].kill()
-      this.particles[i].draw(context)
+    for (var i in vm.particles) {
+      vm.particles[i].update()
+      vm.particles[i].kill()
+      vm.particles[i].draw(vm.context)
     }
-    this.tick++ 
+    vm.tick++ 
   }
 }
 
+
+
 ParticleSystem.prototype.stop = function () {
- window.cancelAnimationFrame(this.frameID) 
+  window.cancelAnimationFrame(this.frameID) 
 }
